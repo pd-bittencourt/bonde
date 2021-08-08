4126699e8708e5c56d18e26d5e1293149f8b4617
Pedro <pedrodmbc@gmail.com>
2021-08-08 20:04:34 -0300

    export default styles

--- src/pages/movies/album_de_memorias.js
+++ src/pages/movies/album_de_memorias.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 
-import { Container } from "./styles";
+import Container from "./styles";
 import ArrowLeft from "../../components/ui/ArrowLeft";
 
 function Movie3() {
